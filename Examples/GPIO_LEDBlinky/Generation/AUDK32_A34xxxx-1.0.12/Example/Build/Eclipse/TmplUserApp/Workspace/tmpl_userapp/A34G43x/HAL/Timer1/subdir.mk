5925d3391abc0d42648e5a5c7d04f509a68be5be
sinancngz <sinan.cengiz@empa.com>
2026-06-26 10:00:05 +0300

    GPIO_LEDBlinky project added

---
+++ Examples/GPIO_LEDBlinky/Generation/AUDK32_A34xxxx-1.0.12/Example/Build/Eclipse/TmplUserApp/Workspace/tmpl_userapp/A34G43x/HAL/Timer1/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+C:/Users/sinan/Desktop/TiremoExmpls/GPIO_LEDBlinky/Generation/AUDK32_A34xxxx-1.0.12/Platform/HAL/Timer1/hal_timer1.c 
+
+OBJS += \
+./HAL/Timer1/hal_timer1.o 
+
+C_DEPS += \
+./HAL/Timer1/hal_timer1.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+HAL/Timer1/hal_timer1.o: C:/Users/sinan/Desktop/TiremoExmpls/GPIO_LEDBlinky/Generation/AUDK32_A34xxxx-1.0.12/Platform/HAL/Timer1/hal_timer1.c HAL/Timer1/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU Arm Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mlittle-endian -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -g3 -DEXTRN_SUBFAMILY_A34G43x -DEXTRN_ABOV_MODULE_CONFIG -I"C:/Users/sinan/Desktop/TiremoExmpls/GPIO_LEDBlinky/Generation/AUDK32_A34xxxx-1.0.12/Example/Build/Eclipse/TmplUserApp/Workspace/tmpl_userapp/../../../../../../Framework/CMSIS/Core/Include" -I"C:/Users/sinan/Desktop/TiremoExmpls/GPIO_LEDBlinky/Generation/AUDK32_A34xxxx-1.0.12/Example/Build/Eclipse/TmplUserApp/Workspace/tmpl_userapp/../../../../../../Framework/CMSIS/Device/ABOV/Include" -I"C:/Users/sinan/Desktop/TiremoExmpls/GPIO_LEDBlinky/Generation/AUDK32_A34xxxx-1.0.12/Example/Build/Eclipse/TmplUserApp/Workspace/tmpl_userapp/../../../../../../ProductConfig/Config" -I"C:/Users/sinan/Desktop/TiremoExmpls/GPIO_LEDBlinky/Generation/AUDK32_A34xxxx-1.0.12/Example/Build/Eclipse/TmplUserApp/Workspace/tmpl_userapp/../../../../../../Platform/HAL/Include" -I"C:/Users/sinan/Desktop/TiremoExmpls/GPIO_LEDBlinky/Generation/AUDK32_A34xxxx-1.0.12/Example/Build/Eclipse/TmplUserApp/Workspace/tmpl_userapp/../../../../../../Platform/HAL/HPL/Include" -I"C:/Users/sinan/Desktop/TiremoExmpls/GPIO_LEDBlinky/Generation/AUDK32_A34xxxx-1.0.12/Example/Build/Eclipse/TmplUserApp/Workspace/tmpl_userapp/../../../../../../Platform/Library/ABOV/Debug/Include" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
